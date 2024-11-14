7262fcaec36dbc8a0c8b826a674af5a794b91b5e
Adrián <acasveg2608@g.educaand.es>
2024-11-14 18:20:07 +0100

    Version: 0.0.4 - SNAPSHOT PROYECTO INICIAL - EDICIÓN XMLS:
    Se modificaron los XML debido a errores de duplicación en las etiquetas.
    
    Se añadió la pantalla de bienvenida con un botón.

--- app/src/main/java/com/example/proyectoesencia/MainActivity.kt
+++ app/src/main/java/com/example/proyectoesencia/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.proyectoesencia.ui.theme.ProyectoESENCIATheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,59 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProyectoESENCIATheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Hola",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppManager(Modifier.fillMaxSize())
             }
         }
     }
 }
 
+// Funcion encargada de re-escalar la pantalla de bienvenida
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppManager(modifier: Modifier = Modifier){
+    Surface(modifier) {
+        PantallaBienvenida()
+    }
+
 }
 
-@Preview(showBackground = true)
+// Función pantalla bienvenida
+
 @Composable
-fun GreetingPreview() {
+fun PantallaBienvenida(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        // Muestra el siguiente texto:
+        Text(stringResource(R.string.text_bienvenida))
+
+        // Muestra un botón con el texto "Empezar" pero no hace nada
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = {}
+        ) {
+            Text(stringResource(R.string.btn_bienvendia_text))
+        }
+    }
+
+}
+//Muestra el preview de la pantalla de bienvenida
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun PreviewPantallaBienvenida() {
     ProyectoESENCIATheme {
-        Greeting("Android")
+        PantallaBienvenida()
+    }
+}
+
+//Muestra el preview de la pantalla de bienvenida al completo
+
+
+@Preview
+@Composable
+fun PreviewPantalla(){
+    ProyectoESENCIATheme {
+        AppManager(Modifier.fillMaxSize())
     }
 }
